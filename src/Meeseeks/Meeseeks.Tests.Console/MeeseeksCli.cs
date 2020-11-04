1919d4607429045cad53b7b41eed3e65014ef3df
Ian Paullin <ianpaullin@gmail.com>
2020-11-04 00:47:57 -0600

    Initial unit testing for MeeseeksCli

---
+++ src/Meeseeks/Meeseeks.Tests.Console/MeeseeksCli.cs
@@ -0,0 +1,37 @@
+using System;
+using Xunit;
+using NDesk.Options;
+
+namespace Meeseeks.Tests
+{
+    public class MeeseeksCli
+    {
+        [Fact]
+        public void Testing_Option_Verbose()
+        {
+            //arrange
+            //act
+            //assert
+            Assert.Equal(0, 0);
+        }
+
+        [Fact]
+        public void Testing_Option_Help()
+        {
+            //arrange
+            //act
+            //assert
+            Assert.Equal(0, 0);
+        }
+
+        [Fact]
+        public void Testing_Option_LogFile()
+        {
+            //arrange
+            //act
+            //assert
+            Assert.Equal(0, 0);
+        }
+
+    }
+}
