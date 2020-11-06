29742bef6e868540a541590db01712c601221eb3
Ian Paullin <ianpaullin@gmail.com>
2020-11-05 22:48:52 -0600

    Adding Configuration class and unit test

---
+++ src/Meeseeks/Meeseeks.Library/Configuration.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+using Microsoft.Extensions.Configuration.CommandLine;
+using Microsoft.Extensions.Configuration.EnvironmentVariables; 
+
+namespace Meeseeks.Library
+{
+    class Configuration
+    {
+        public Configuration(string[] args)
+        {
+            IConfiguration Configuration = new ConfigurationBuilder()
+              .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+              .AddEnvironmentVariables()
+              .AddCommandLine(args)
+              .Build();
+        }
+    }
+}
